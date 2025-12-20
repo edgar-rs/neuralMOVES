1d71544ea54a1e841d8ae784d9018595fd94315b
edgar-rs <60362393+edgar-rs@users.noreply.github.com>
2025-12-20 15:04:29 -0600

    Update __init__.py to include emissions calculation

--- src/neuralmoves/__init__.py
+++ src/neuralmoves/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2024-present Edgar Ramirez Sanchez <edgarrs@mit.edu>
 #
 # SPDX-License-Identifier: MIT
+
+from .__about__ import __version__
+from .emission_calculation import get_emissions
+
+__all__ = ["get_emissions", "__version__"]
