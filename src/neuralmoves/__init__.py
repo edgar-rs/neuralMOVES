a19d072020cc12c0743982d11a928254d37b4890
edgar-rs <60362393+edgar-rs@users.noreply.github.com>
2025-12-20 21:42:37 -0600

    Refactor imports and update __all__ exports

--- src/neuralmoves/__init__.py
+++ src/neuralmoves/__init__.py
@@ -1,8 +1,17 @@
-# SPDX-FileCopyrightText: 2024-present Edgar Ramirez Sanchez <edgarrs@mit.edu>
-#
-# SPDX-License-Identifier: MIT
-
 from .__about__ import __version__
-from .emission_calculation import get_emissions
+from .api import (
+    estimate_running_co2,
+    list_available_models,
+    idling_rate,
+    get_expected_error,
+)
+from .config import VALID_MODEL_YEARS
 
-__all__ = ["get_emissions", "__version__"]
+__all__ = [
+    "__version__",
+    "estimate_running_co2",
+    "list_available_models",
+    "idling_rate",
+    "get_expected_error",
+    "VALID_MODEL_YEARS",
+]
