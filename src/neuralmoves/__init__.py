7ab4e1e296de64eba78e4b66004f617921871b2b
edgar-rs <60362393+edgar-rs@users.noreply.github.com>
2025-12-24 00:25:50 -0600

    Update exports in __init__.py for emissions estimation

--- src/neuralmoves/__init__.py
+++ src/neuralmoves/__init__.py
@@ -1,8 +1,16 @@
+"""
+NeuralMOVES: Lightweight microscopic surrogate of EPA MOVES for vehicle CO₂ emissions.
+
+This package provides fast, accurate emission predictions using neural network
+surrogates trained on EPA MOVES output.
+"""
+
 from .__about__ import __version__
 from .api import (
     estimate_running_co2,
-    list_available_models,
+    estimate_emissions_timeseries,
     idling_rate,
+    list_available_models,
     get_expected_error,
 )
 from .config import VALID_MODEL_YEARS
@@ -10,8 +18,9 @@ from .config import VALID_MODEL_YEARS
 __all__ = [
     "__version__",
     "estimate_running_co2",
-    "list_available_models",
+    "estimate_emissions_timeseries",
     "idling_rate",
+    "list_available_models",
     "get_expected_error",
     "VALID_MODEL_YEARS",
 ]
