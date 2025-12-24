d2acdf529600113824f32d3ced54b17f1dcd67b5
edgar-rs <60362393+edgar-rs@users.noreply.github.com>
2025-12-24 00:25:21 -0600

    Update version number to 0.3.0

--- src/neuralmoves/__about__.py
+++ src/neuralmoves/__about__.py
@@ -1,5 +1,3 @@
-# SPDX-FileCopyrightText: 2024-present Edgar Ramirez Sanchez <edgarrs@mit.edu>
-#
-# SPDX-License-Identifier: MIT
-__all__ = ["__version__"]
-__version__ = "0.2.0"
+"""Package metadata for neuralmoves."""
+
+__version__ = "0.3.0"
